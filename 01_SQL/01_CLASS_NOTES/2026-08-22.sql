d1e38e875ae690349897036469f36296defcfb12
Arpit Bangre <arpit.m.bangre@gmail.com>
2026-08-23 21:33:50 +0530

    sync(dashboard): update Sunday night recovery schedule and sync Vercel

--- 01_SQL/01_CLASS_NOTES/2026-08-22.sql
+++ 01_SQL/01_CLASS_NOTES/2026-08-22.sql
@@ -12,3 +12,33 @@
 -- 1 row number 
 -- 2 dense rank 
 -- 3 rank
+
+-- rank 
+-- the column should be in ordered way that is sorted 
+-- important for interview without writing query explain the output 
+-- used in company at good level 
+/*
+marks desc order	row_number position   dense_rank value    rank value position 
+99					1						1				   1
+99                  2                       1				   1
+98                  3                       2				   3 
+98                  4                       2
+98                  5                       2
+94                  6 
+90
+90
+85
+85
+85
+80
+80
+70
+70                 15                        7                14
+null               16						 8				  16
+-- exam rank system used here 
+-- null also count in that 
+-- group by we can't use here since need aggregate function 
+
+-- here we use partition by 
+-- used with window function + aggregate as well 
+*/
