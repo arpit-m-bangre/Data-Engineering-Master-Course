88ee350ce5206d806e5191cf0cf7170025724f99
Arpit Bangre <arpit.m.bangre@gmail.com>
2026-08-23 18:25:10 +0530

    sync: push dashboard public tasks and initial class 19 raw notes

---
+++ 01_SQL/01_CLASS_NOTES/2026-08-22.sql
@@ -0,0 +1,14 @@
+-- windows functions 
+
+-- windows functions are imp but not de point of it is used by da mostly 
+
+-- so many scenario based question create here 
+
+-- 1 ranking -- most imp 
+-- 2 value based 
+-- 3 aggregate 
+
+-- Ranking 
+-- 1 row number 
+-- 2 dense rank 
+-- 3 rank
